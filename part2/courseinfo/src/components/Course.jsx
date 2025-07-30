8cfa752f0647db505200c36928d722174c260bcf
Annika <makelannika@gmail.com>
2025-07-30 21:02:24 +0300

    exercises 2.1-2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,36 +1,38 @@
 const Course = ({ course }) => (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Content = ({ parts }) => (
+  <ul>
+    {parts.map(part => (
+      <li key={part.id}>
+        <Part part={part} />
+      </li>
+    ))}
+  </ul>
+)
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.exercises,
+    0,
   )
-  
-  const Header = ({ name }) => <h1>{name}</h1>
-  
-  const Content = ({ parts }) => (
-    <div>
-      {parts.map(part => (
-          <Part key={part.id} part={part} />
-        ))}
-    </div>
+
+  return (
+    <p>Total of {sum} exercises</p>
   )
-  
-  const Part = ({ part }) => (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-  
-  const Total = ({ parts }) => {
-    const sum = parts.reduce(
-      (accumulator, currentValue) => accumulator + currentValue.exercises,
-      0,
-    )
-  
-    return (
-      <p>Total of {sum} exercises</p>
-    )
-  }
+}
 
 export default Course
