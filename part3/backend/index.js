b32dca79d785c43629f471b885aa175e40d87b8f
Annika <makelannika@gmail.com>
2026-03-23 13:07:56 +0200

    uibuild

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -69,8 +69,14 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
+    const person = persons.find(p => p.id === id)
+    
+    if (!person) {
+        return response.status(404).end()
+    }
 
+    persons = persons.filter(p => p.id !== id)
+    
     response.status(204).end()
 })
 
