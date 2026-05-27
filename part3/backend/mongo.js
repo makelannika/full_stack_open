ce3151edb4936efe68db16b06b967b9cfba6384a
Annika <makelannika@gmail.com>
2026-05-27 19:23:01 +0300

    exercise 3.12

---
+++ part3/backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.gbz1klt.mongodb.net/phonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  important: Boolean,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {  
+  Person.find({}).then(result => {
+      result.forEach(note => {
+          console.log(note)
+        })
+        mongoose.connection.close()
+      })
+    }
+    
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+    important: false,
+  })
+  
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
