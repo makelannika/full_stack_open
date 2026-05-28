8929a9a341d1d83b3673895c791ed6940cd18993
Annika <makelannika@gmail.com>
2026-05-28 10:16:30 +0300

    exercise 3.12

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -20,10 +20,11 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length === 3) {  
+if (process.argv.length === 3) {
+  console.log('phonebook:')
   Person.find({}).then(result => {
-      result.forEach(note => {
-          console.log(note)
+      result.forEach(person => {
+          console.log(person.name, person.number)
         })
         mongoose.connection.close()
       })
