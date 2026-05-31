c1eef3987f19e6a6f6ba147525ab306ea0412b99
Annika <makelannika@gmail.com>
2026-05-31 18:21:41 +0300

    exercise 3.16

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -92,7 +92,7 @@ app.put('/api/persons/:id', (request, response, next) => {
             error: 'Name and number required'
         })
     }
-    
+
     Person.findById(request.params.id)
         .then(person => {
             if (!person) {
@@ -108,6 +108,18 @@ app.put('/api/persons/:id', (request, response, next) => {
         })
         .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
