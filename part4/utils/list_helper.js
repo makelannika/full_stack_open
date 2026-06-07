64a586a99cfcb58cb2d8306e39d911bcc666a940
Annika <makelannika@gmail.com>
2026-06-07 18:27:53 +0300

    exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,7 +10,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0)
+    return null
+
+  const reducer = (favorite, blog) => {
+    return favorite.likes > blog.likes ? favorite : blog
+  }
+
+  return blogs.reduce(reducer, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
