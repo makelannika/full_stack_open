f915d3b561b205eb6d8c6fe0bf3830c23c173d66
Annika <makelannika@gmail.com>
2026-08-26 19:38:18 +0300

    ex 5.13-5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,16 +1,45 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author', () => {
-  const blog = {
-    url: 'test url',
-    title: 'test title',
-    author: 'test author',
+const blog = {
+  url: 'url',
+  title: 'title',
+  author: 'author',
+  likes: 5,
+  user: {
+    username: 'user',
+    name: 'user',
+    token: 'token'
   }
+}
 
+test('renders title and author by default', () => {
   render(<Blog blog={blog} />)
 
-  const element = screen.getByText('test title test author')
+  const titleAndAuthorElement = screen.getByText('title author')
+  const urlElement = screen.queryByText('url')
+  const likesElement = screen.queryByText('likes 5')
 
-  expect(element).toBeDefined()
+  expect(titleAndAuthorElement).toBeDefined()
+  expect(urlElement).toBeNull()
+  expect(likesElement).toBeNull()
+})
+
+test('renders url likes and user when view button is clicked', async () => {
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const urlElement = screen.getByText('url')
+  const likesElement = screen.getByText('likes 5')
+  const userElement = screen.getByText('user')
+
+  expect(urlElement).toBeDefined()
+  expect(likesElement).toBeDefined()
+  expect(userElement).toBeDefined()
+
+  screen.debug()
 })
--- part5/testSetup.js
+++ part5/testSetup.js
@@ -1,6 +1,17 @@
-import { afterEach } from 'vitest'
+import { afterEach, beforeEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+
+beforeEach(() => {
+  window.localStorage.setItem(
+    'loggedUser',
+    JSON.stringify({
+      username: 'user',
+      name: 'user',
+      token: 'token',
+    })
+  )
+})
 
 afterEach(() => {
   cleanup()
